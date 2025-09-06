cc0ccf8a932d1f2cf2dea1bba6ca0a80c08bdccb
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-09-06 23:48:01 +0530

    Add Getter & Setter

--- LearningRESTAPIs/src/main/java/com/codingshuttle/learnSpringBoot/LearningRESTAPIs/entity/Student.java
+++ LearningRESTAPIs/src/main/java/com/codingshuttle/learnSpringBoot/LearningRESTAPIs/entity/Student.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Student {
 
     @Id
@@ -14,6 +18,7 @@ public class Student {
     private Long id;
 
     private String name;
+
     private String email;
 
 }
