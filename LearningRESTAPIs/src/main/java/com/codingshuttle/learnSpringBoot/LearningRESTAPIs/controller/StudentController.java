62fac9257dc021779bf855f4420e1b8576928c5a
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-09-10 22:53:11 +0530

    Add config & ModelMapper dependency

--- LearningRESTAPIs/src/main/java/com/codingshuttle/learnSpringBoot/LearningRESTAPIs/controller/StudentController.java
+++ LearningRESTAPIs/src/main/java/com/codingshuttle/learnSpringBoot/LearningRESTAPIs/controller/StudentController.java
@@ -2,30 +2,30 @@ package com.codingshuttle.learnSpringBoot.LearningRESTAPIs.controller;
 
 
 import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.dto.StudentDto;
-import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.entity.Student;
-import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.repository.StundentRepository;
+import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
 @RestController
+@RequiredArgsConstructor
 public class StudentController {
 
-    private final StundentRepository stundentRepository;
+    private final StudentService studentService;
 
-    public StudentController(StundentRepository stundentRepository) {
-        this.stundentRepository = stundentRepository;
+
+
+    @GetMapping("/Students")
+    public List<StudentDto> getAllStudent(){
+        return studentService.getAllStudent();
     }
 
-    @GetMapping("/Student")
-    public List<Student> getStudent(){
-        return stundentRepository.findAll();
-    }
 
-
-    @GetMapping("/Student/{id}")
-    public StudentDto getStudentById() {
-        return new StudentDto(4L, "Prince", "Prince@gmail.com");
+    @GetMapping("/Students/{id}")
+    public StudentDto getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
     }
 }
--- LearningRESTAPIs/src/main/java/com/codingshuttle/learnSpringBoot/LearningRESTAPIs/service/impl/StudentServiceImpl.java
+++ LearningRESTAPIs/src/main/java/com/codingshuttle/learnSpringBoot/LearningRESTAPIs/service/impl/StudentServiceImpl.java
@@ -5,6 +5,7 @@ import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.entity.Student;
 import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.repository.StundentRepository;
 import com.codingshuttle.learnSpringBoot.LearningRESTAPIs.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,14 +16,22 @@ import java.util.List;
 public class StudentServiceImpl implements StudentService {
 
     private final StundentRepository stundentRepository;
+    private final ModelMapper modelMapper;
 
 
     @Override
     public List<StudentDto> getAllStudent() {
         List<Student> students = stundentRepository.findAll();
-        List<StudentDto> studentDtoList = students
+        return students
                 .stream()
-                .map(student -> new StudentDto(student.getId(), student.getName(), student.getEmail())).toList();
-        return List.of();
+                .map(student -> modelMapper.map(student, StudentDto.class))
+                .toList();
+
+    }
+
+    @Override
+    public StudentDto getStudentById(Long id) {
+        Student student = stundentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Student is not found with ID: " +id));
+        return modelMapper.map(student, StudentDto.class);
     }
 }
