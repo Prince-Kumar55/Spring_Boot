2a96ca8389d6deb65cf29a9dfcb1c7f73fc5f396
Prince-Kumar55 <princekumar2392006@gmail.com>
2025-09-17 17:05:33 +0530

    Use @Table -(@UniqueConstraint, @Index) Notation

--- hospitalManagement/src/main/java/com/learnSpring/DataJPA/hospitalManagement/entity/Patient.java
+++ hospitalManagement/src/main/java/com/learnSpring/DataJPA/hospitalManagement/entity/Patient.java
@@ -1,10 +1,7 @@
 package com.learnSpring.DataJPA.hospitalManagement.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,12 +13,24 @@ import java.time.LocalDateTime;
 @ToString
 @Getter
 @Setter
+@Table(
+        name = "Patient_tbl",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "unique_patient_email", columnNames = {"email"}),
+                @UniqueConstraint(name = "unique_patient_name_birthDate", columnNames = {"name", "birthDate"})
+
+        },
+        indexes = {
+                @Index(name = "idx_patient_birth_date", columnList = "birthDate")
+        }
+)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column()
     private String name;
 
     @ToString.Exclude
